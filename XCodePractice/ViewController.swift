4819c79d437aa9462f95137a278d93ed1b042ce5
Ajeatsa <ajeatsa@jhkelly.com>
2017-10-30 10:38:51 -0700

    commit1

--- XCodePractice/ViewController.swift
+++ XCodePractice/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    //label text originally says "Hello World!", color black
+    var tapCount = 0
+    
+    @IBAction func button1(_ sender: Any) {
+        firstLabel.text = "Hello There!"
+        firstLabel.textColor = UIColor.red
+        firstLabel.backgroundColor = UIColor.black
+        tapCount = tapCount+1
+        print (tapCount)
+        if tapCount >= 10 {
+            firstLabel.text = "You've tapped 10 times!"
+        }
+        //Change text to "Hello There!", changes text color to red, changes background to black on label.
+    }
+    @IBAction func button2(_ sender: Any) {
+        firstLabel.text = "Buttons are Cool!"
+        print("ButtonPushed")
+        firstLabel.textColor = UIColor.green
+        //Change text to "Buttons are Cool!", text color green.
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        view.self.backgroundColor = UIColor.black
+        
+         firstLabel.textColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
